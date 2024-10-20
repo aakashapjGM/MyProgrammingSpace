f6cf1635d1fdee62f9ee0c026cc8e81c3c528768
aakashapjGM <aakashapjgm@gmail.com>
2024-10-20 10:19:00 +0000

    Added C# First Project and working on GrayScale img

---
+++ CSharp_Programming/00_FirstProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args, Console console)
+        {
+            string condition = console.ReadLine();
+
+            if (condition == "A"){
+                console.WriteLine("Hello Aakash");
+            }else if (condition == "B")
+                console.WriteLine("Hello Bera");
+            
+        }
+    }
+}
