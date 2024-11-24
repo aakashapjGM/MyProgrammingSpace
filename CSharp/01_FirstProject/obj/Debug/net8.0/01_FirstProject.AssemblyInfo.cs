6270cd059ced7312233cdad4b4d2dee752fce654
Aakash <aakashapj@gmail.com>
2024-11-24 20:43:23 +0530

    Worked on Morphological Operators

--- CSharp/01_FirstProject/obj/Debug/net8.0/01_FirstProject.AssemblyInfo.cs
+++ CSharp/01_FirstProject/obj/Debug/net8.0/01_FirstProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("01_FirstProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7594521d5ee0bed148a5dfdce819fc1fd9c0fd63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d053b240010e9dc5c2f34a58f79269a106b1c662")]
 [assembly: System.Reflection.AssemblyProductAttribute("01_FirstProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("01_FirstProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
